4b0e45fd85e2e2a6698921dbfd520744fa71d15a
Zoubiyun <2027625649@qq.com>
2024-04-18 14:46:12 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import '@/assets/css/global.css'
+
+// createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app')
