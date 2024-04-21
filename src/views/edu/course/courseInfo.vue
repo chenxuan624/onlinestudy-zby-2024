bd72daadd2ee293f7ce656b4455516c67a3736db
Zoubiyun <2027625649@qq.com>
2024-04-21 16:24:45 +0800

    添加课程基本信息已完成

--- src/views/edu/course/courseInfo.vue
+++ src/views/edu/course/courseInfo.vue
@@ -10,12 +10,10 @@
             <el-step title="最终发布"/>
         </el-steps>
 
-
-
         <el-form label-width="120px">
 
-            <el-form-item label="课程标题">
-                <el-input v-model="courseInfo.title" placeholder=" 示例：机器学习项目课：从基础到搭建项目视频课程。专业名称注意大小写"/>
+            <el-form-item label="课程标题" :rules="[{ required: true, message: '请输入课程标题', trigger: 'blur' }]">
+                <el-input v-model="courseInfo.title" placeholder=" 示例：Java SE基础教学"/>
             </el-form-item>
 
             <!-- 课程分类 -->
@@ -42,31 +40,13 @@
                 </el-select>
             </el-form-item>
 
-            <!-- 课程讲师 -->
-            <el-form-item label="课程讲师">
-                <el-select
-                    v-model="courseInfo.teacherId"
-                    placeholder="请选择">
-                    <el-option
-                    v-for="teacher in teacherList"
-                    :key="teacher.id"
-                    :label="teacher.name"
-                    :value="teacher.id"/>
-                </el-select>
-            </el-form-item>
-
-
-            <!-- <el-form-item label="总课时">
-                <el-input-number :min="0" v-model="courseInfo.lessonNum" controls-position="right" placceholder="请填写课程的总课时数"/>
-            </el-form-item> -->
-
             <!-- 课程简介  -->
-            <el-form-item label="课程简介">
+            <el-form-item label="课程简介" :rules="[{ required: true, message: '请输入课程简介', trigger: 'blur' }]">
                 <el-input v-model="courseInfo.description" :rows="6" type="textarea" placeholder=" "/>
             </el-form-item>
 
             <!-- 课程封面-->
-            <el-form-item label="课程封面(比例尽量控制在2：3)">
+            <el-form-item label="课程封面">
                 <el-upload
                     :show-file-list="false"
                     :on-success="handleAvatarSuccess"
@@ -76,6 +56,7 @@
                     <img
                     style="height: 200px; width: 300px;"
                      :src="courseInfo.cover">
+                  <div slot="tip" class="el-upload__tip">点击图片更换封面,封面图比例尽量控制在3:2</div>
                 </el-upload>
             </el-form-item>
             <el-form-item>
@@ -95,6 +76,8 @@ export default {
     name:'CourseInfo',
     data() {
         return {
+          userInfo:{},
+          role:'',
             saveBtnDisabled: false, // 保存按钮是否禁用
 
             courseInfo: {
@@ -102,6 +85,8 @@ export default {
                 subjectId: '',//二级分类id
                 subjectParentId:'',//一级分类id
                 teacherId: '',
+                applicationStatus: 0,
+                checkStatus: 0,
                 lessonNum: 0,
                 viewCount: 0,
                 commentCount: 0,
@@ -111,35 +96,19 @@ export default {
 
             courseId: '',
 
-            teacherList: [], // 封装所有的讲师
-
             subjectOneList: [], // 一级分类集合
-
-
 
             subjectTwoList: [], // 二级分类集合
 
-            BASE_API: process.env.BASE_API, // 接口API地址
+            BASE_API: 'http://localhost:8008', // 接口API地址
         }
     },
     created() {
 
-        /* // 获取到路由当中的id值
-        if (this.$route.params && this.$route.params.id) { // 修改操作
-            this.courseId = this.$route.params.id
+      this.userInfo = JSON.parse(localStorage.getItem("userInfo"))
+      this.role = localStorage.getItem("role")
 
-            // 调用
-            this.getInfo()
-        } else { // 添加操作
-            // 初始化所有讲师
-            this.getListTeacher()
-
-            // 初始化一级分类
-            this.getOneSubject()
-        } */
-
-        this.init()
-
+      this.init()
 
     },
 
@@ -155,22 +124,12 @@ export default {
             // 获取到路由当中的id值
             if (this.$route.params && this.$route.params.id) { // 修改操作
                 this.courseId = this.$route.params.id
-
                 // 调用
                 this.getInfo()
             } else { // 添加操作
-                // 初始化所有讲师
-                this.getListTeacher()
-
                 // 初始化一级分类
                 this.getOneSubject()
-
-                this.courseInfo = {
-                    cover: 'https://onlinestudy-useravatar-biyunzou.oss-cn-hangzhou.aliyuncs.com/default/JavaCourseDefaultCover.png',
-                    lessonNum: 0,
-                    viewCount: 0,
-                    commentCount: 0
-                }
+                this.courseInfo.cover = 'https://onlinestudy-useravatar-biyunzou.oss-cn-hangzhou.aliyuncs.com/default/JavaCourseDefaultCover.png'
             }
         },
 
@@ -197,7 +156,7 @@ export default {
                             }
                         })
                         // 初始化所有讲师
-                        this.getListTeacher()
+                        // this.getListTeacher()
                 })
         },
 
@@ -209,15 +168,15 @@ export default {
         // 上传封面之前，可以规定文件类型和文件大小
         beforeAvatarUpload(file) {
             const isJPG = file.type === 'image/jpeg'
-            const isLt2M = file.size / 1024 / 1024 < 2 // TODO 有bug
+            const isLt10M = file.size / 1024 / 1024 < 10
 
             if (!isJPG) {
                 this.$message.error('上传头像图片只能是 JPG 格式!')
             }
-            if (!isLt2M) {
-                this.$message.error('上传头像图片大小不能超过 2MB!')
+            if (!isLt10M) {
+                this.$message.error('上传头像图片大小不能超过 10MB!')
             }
-            return isJPG && isLt2M
+            return isJPG && isLt10M
         },
 
         // 当选择了某一个一级分类后会触发此方法，目的时为了找到当前一级分类底下的二级分类
@@ -246,26 +205,26 @@ export default {
                 })
         },
 
-        // 查询所有讲师
-        getListTeacher() {
-            course.getAllTeacher()
-                .then( response => {
-                    this.teacherList = response.data.items
-                })
-        },
-
         // 添加课程
         addCourse() {
+          this.courseInfo.teacherId = this.userInfo.id
+          console.log(this.courseInfo)
             course.addCourseInfo(this.courseInfo)
                 .then( response => {
-
+                  if (response.success) {
                     // 提示信息
                     this.$message({
-                        type: 'success',
-                        message: '添加课程基础信息成功!'
+                      type: 'success',
+                      message: '添加课程基础信息成功!'
                     })
                     // 跳转到第二步
                     this.$router.push({ path: '/course/chapter/' + response.data.courseId })
+                  }else{
+                    this.$message({
+                      type: 'error',
+                      message: response.message
+                    })
+                  }
                 })
         },
 
@@ -284,9 +243,15 @@ export default {
         },
 
         saveOrUpdate() {
-            if (this.courseInfo.id) { // 有id,修改操作
-                this.updateCourse()
-            }else { // 无id，添加操作
+          if (this.courseInfo.title === ''){
+            this.$message.error("课程标题不能为空")
+          }else if(this.courseInfo.subjectParentId === '' || this.courseInfo.subjectId === ''){
+            this.$message.error("课程分类不能为空")
+          }else if (this.courseInfo.description === ''){
+            this.$message.error("课程简介不能为空")
+          }else if (this.courseInfo.id) { // 有id,修改操作
+             this.updateCourse()
+          }else { // 无id，添加操作
                 this.addCourse()
             }
         }
